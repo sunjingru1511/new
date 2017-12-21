46b4a5803a217c498035c90c90bdfa06b6ae3bfd
sunjingru1511 <1813975672@qq.com>
2017-12-21 11:05:13 +0800

    dd

---
+++ app/controller/conController.js
@@ -0,0 +1,8 @@
+app.controller("conController", function($scope){
+    $scope.door="景点门票";
+    $scope.son="酒店";
+    $scope.son1="周边自由行";
+    $scope.son2="民宿.客栈";
+    $scope.son3="周边跟团游";
+    $scope.piao=["机票", "火车票", "汽车票"];
+})
